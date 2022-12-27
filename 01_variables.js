d16f0833c1d5e85916bba632484e7254c9eaf845
Atif Raza <razaatif20786@gmail.com>
2022-12-27 21:36:39 +0530

    Initial commit

---
+++ 01_variables.js
@@ -0,0 +1,42 @@
+"use strict";
+//declare a variable
+
+var firstName = "Atif";
+
+console.log(firstName); //use a variable
+
+// change Value
+
+var firstName = "Ram";
+
+console.log(firstName);
+
+// Rules for naming variable
+
+// you cannot start with Number
+
+// example
+//  1value(invalid)
+//   var 1value = 10
+
+//  value1(valid)
+var value1 = 10;
+console.log(value1);
+
+// you can use only underscore _ or dollar symbol
+// first_name(valid)
+// _firstname(valid)
+
+// first$name(valid)
+// $firstname(valid)
+
+// you cannot use spaces
+
+// first name(invalid)
+
+let myName = "Atif";
+console.log(myName);
+
+// declare constants
+// cannot change the value of variable declared with const keyword
+const pi = 3.14; 
